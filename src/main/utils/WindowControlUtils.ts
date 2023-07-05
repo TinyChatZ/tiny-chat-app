e3db781c87c5089259f9fdca271b7c385f3a661a
Jaken <841024639@qq.com>
2023-07-05 14:27:01 +0800

    🐛 FIX: 修复win10高分屏画面拖动无故缩放 关于历史记录、多会话管理思路 #19

--- src/main/utils/WindowControlUtils.ts
+++ src/main/utils/WindowControlUtils.ts
@@ -1,12 +1,15 @@
 import { BrowserWindow, Point, screen } from 'electron'
 import { AbstractWindow } from '../windows/AbstractWindow'
-
+/**
+ * 窗口移动处理类
+ */
 export class WindowMove {
   interval
   winPos: Array<number>
   mousePos: Point
   window: BrowserWindow
   timeout = 10
+  curSize = [0, 0]
   static windowMoveMap = new Map<AbstractWindow, WindowMove>()
 
   constructor(absWindow: AbstractWindow, timeout?: number) {
@@ -17,22 +20,37 @@ export class WindowMove {
     this.mousePos = { x: 0, y: 0 }
     WindowMove.windowMoveMap.set(absWindow, this)
   }
-
+  /**
+   * 用于获取WindowMove实例
+   * 同一个抽象窗口只会获得单例
+   * @param absWindow 抽象窗口
+   * @returns
+   */
   static getInstance(absWindow: AbstractWindow): WindowMove {
     const windowMove = this.windowMoveMap.get(absWindow)
     if (windowMove) return windowMove
     return new WindowMove(absWindow)
   }
-
+  /**
+   * 开始移动，是的窗口跟随鼠标位置
+   * 移动期间静止
+   */
   startMove(): void {
     this.winPos = this.window.getPosition()
     this.mousePos = screen.getCursorScreenPoint()
+    this.curSize = this.window.getSize()
     clearInterval(this.interval)
     this.interval = setInterval(() => {
       const curPos = screen.getCursorScreenPoint()
       const x = this.winPos[0] + curPos.x - this.mousePos.x
       const y = this.winPos[1] + curPos.y - this.mousePos.y
-      this.window.setPosition(x, y, true)
+      console.log(this.curSize)
+      this.window.setBounds({
+        width: this.curSize[0],
+        height: this.curSize[1],
+        x: x,
+        y: y
+      })
     }, this.timeout)
   }
 
